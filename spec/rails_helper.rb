f1f4e4ecd6eb4eb13449083faa4f093ee5682900
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-11-04 13:15:09 +0100

    Add factory bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'support/database_cleaner'
+require 'support/factory_bot'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
