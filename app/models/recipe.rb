64ffb0ea1766d96c252c7626715dbcc94a6fa6e9
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-11-04 16:55:57 +0100

    Minor fixes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+  validates :title, presence: true
+  validates :slug, presence: true
 
   def generate_slug
     (title + ',' + [*('a'..'z'),*('0'..'9')].shuffle[0,8].join).parameterize
