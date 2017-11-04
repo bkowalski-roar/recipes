804379a197a86525553a6285a674fa5ceeb822c2
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-11-04 15:57:00 +0100

    Create sees file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+recipes = [
+    Recipe.create(title: 'Duńska zupa z porów', description: 'Przepis na duńską zupę porową, która podawana jest z serem, grzankami lub bagietką. Jej przygotowanie nie zajmie dużo czasu, a na zimne jesienne dni zda się w sam raz.'),
+    Recipe.create(title: 'Czerwony barszcz według mojej babci Reginy', description: 'Tak jest od wieków, że potrawy wigilijne, zwłaszcza te najbardziej tradycyjne robią babcie i... dziadkowie. Jak kiedyś będę dziadkiem, to moim wnukom dam to, co najlepsze - czyli prawdziwy smak na kol'),
+    Recipe.create(title: 'Zupa z dyni z imbirem bardzo aromatyczna', description: 'Zupa z dyni z imbirem jest szalenie aromatyczna. Kolorowa, delikatna, jedwabista w smaku. Warto wypróbować, szczególnie w jesienne dni!'),
+    Recipe.create(title: 'Gesta zupa z brokułami i żóltym serem', description: 'Bardzo gesta, kremowa i kolorowa zupa - w sam raz na zimowy obiad'),
+    Recipe.create(title: 'Zupa ziemniakowa bardzo jedwabista i mocno aromatyczna', description: 'Delikatnie aksamitna zupa ziemniaczana, która rozgrzeje Cię po jesiennym spacerze. Zupa jest aromatyczna i bardzo jedwabista. Jak ją zrobić? Zobacz nasz przepis na zupę ziemniaczaną. '),
+    Recipe.create(title: 'Żur Wielkanocny, mocno wędzonką i majerankiem pachnący', description: 'Tem żur, to obowiązkowa pozycja na wielkanocnym stole. Pachnie mocno wędzonką i majerankiem, jest kwaśny i aromatyczny. Mocno rozgrzewa i wspaniale podkręca apetyt!'),
+    Recipe.create(title: 'Zupa z kurek - nie znam lepszej :)', description: 'Każdy będzie zadowolony :) Jak świat światem, są dwie frakcje: makaronowa i ziemniakowa. Pierwsi uważają, że grzybowa zupa to tylko z makaron, ci drudzy nie wyobrażając sobie z czymś innym, niż z...'),
+    Recipe.create(title: 'Rosół domowy', description: 'Jeśli ma być taki "jak u Babci", musi być przygotowany z kury wiejskiej, kupionej na targu, lub otrzymanej w prezencie od rodziny ze wsi. Drób pochodzący z fermy zupełnie się do niego nie nadaje. Pods...'),
+    Recipe.create(title: 'Zimowy tradycyjny kapuśniak polski', description: 'Zimowy tradycyjny kapuśniak, to typowo polskie danie, które musi być tłuste, aromatyczne i mocno kwaśne. Pysznie rozgrzewa.'),
+    Recipe.create(title: 'Zupa pomidorowa z pieczonych pomidorów - najlepsza na świecie :) - film video HD', description: 'Zupa pomidorowa z pieczonych pomidorów jest po prostu najlepsza na świecie. Głęboki smak pomidorów i czosnku sprawia, że zupa jest bardzo aromatyczna. Do tego tymianek i świeża bazylia, a także pieczo...')
+]
+
+p 'seed success!'
