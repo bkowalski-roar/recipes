a4761605524c55e978b70ede3386b3c11243f098
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-11-05 23:21:09 +0100

    Create route for recipe creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :recipes, only: [:index, :show], param: :slug do
+      resources :recipes, only: [:index, :show, :create], param: :slug do
         resources :stages, only: :show do
           resources :steps, only: :index
           resources :ingredients, only: :index
