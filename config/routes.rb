d60487ad797d42ee50f3ba3d5bbdf40031f84a46
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-11-05 15:00:55 +0100

    Add steps to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :recipes, only: [:index, :show], param: :slug do
-        resources :stages, only: :show
+        resources :stages, only: :show do
+          resources :steps, only: :index
+        end
       end
     end
   end
