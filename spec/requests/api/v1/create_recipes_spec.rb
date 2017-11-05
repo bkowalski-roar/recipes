751597d75663757b49ca718f0ffb921c93305561
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-11-05 23:49:55 +0100

    Fix create recipe spec

--- spec/requests/api/v1/create_recipes_spec.rb
+++ spec/requests/api/v1/create_recipes_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 describe 'Create recipe api' do
   it 'creates plain recipe' do
-    post '/api/v1/recipes'
+    expect{ post '/api/v1/recipes', params: { title: 'sth' } }.to change{ Recipe.count }.from(0).to(1)
 
     expect(response).to be_succes
     expect(json['title']).not_to be_nil
     expect(json['slug']).to be_nil
     expect(json['stages']).not_to be_nil
     expect(json['created_at']).not_to be_nil
-    expect(response).to change{ Recipe.count }.from(0).to(1)
   end
+
+  
 end
