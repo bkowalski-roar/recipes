c52cd37da3632ff9dec8816489805d07a83097be
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-11-05 23:49:15 +0100

    Move slug to application controller

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def generate_slug seed
+    (seed + ',' + [*('a'..'z'),*('0'..'9')].shuffle[0,8].join).parameterize
+  end
 end
