f76b9b183f1231a39878744d6c420895eefafe85
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-11-05 23:21:03 +0100

    Spec update

--- spec/requests/api/v1/create_recipes_spec.rb
+++ spec/requests/api/v1/create_recipes_spec.rb
@@ -9,5 +9,6 @@ describe 'Create recipe api' do
     expect(json['slug']).to be_nil
     expect(json['stages']).not_to be_nil
     expect(json['created_at']).not_to be_nil
+    expect(response).to change{ Recipe.count }.from(0).to(1)
   end
 end
