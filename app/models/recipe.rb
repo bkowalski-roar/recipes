50ff8f64300762484541628dc53e2c54d68003b7
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-11-05 23:14:04 +0100

    Create scope for visible recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,8 @@ class Recipe < ApplicationRecord
   validates :title, presence: true
   validates :slug, presence: true
 
+  scope :visible, -> { where(published: true) }
+
   def generate_slug
     (title + ',' + [*('a'..'z'),*('0'..'9')].shuffle[0,8].join).parameterize
   end
