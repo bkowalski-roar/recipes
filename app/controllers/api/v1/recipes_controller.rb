c3f7c0fd8ea9254a282d65daae24f72cccba3675
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-11-05 23:50:07 +0100

    Add create action in RecipeController

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -10,6 +10,17 @@ module Api
         render json: Recipe.visible.find_by(slug: params[:slug])
       end
 
+      def create
+        recipe = Recipe.create!(create_recipe_params)
+        render json: recipe
+      end
+
+      private
+
+      def create_recipe_params
+        params.permit(:title, :description)
+      end
+
     end
   end
 end
